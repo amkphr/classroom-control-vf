fa5f90c22a0d057a3234319a52bdd148929bbbfa
amkphr <andrew.mohebbi.kphr@statefarm.com>
2016-05-24 10:20:57 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
-  path => '/usr/bin/usr/local/bin',
+ path => '/usr/bin:/usr/local/bin',
   creates => '/etc/motd', 
 }
   }
