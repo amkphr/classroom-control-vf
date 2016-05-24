53a4758540a1cac90d05817f876e2c28e1ec2ce4
amkphr <andrew.mohebbi.kphr@statefarm.com>
2016-05-24 09:13:41 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,9 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-  file { '/etc/motd':
+  }
+  
+file { '/etc/motd':
   ensure => file,
   owner => 'root',
   group => 'root',
