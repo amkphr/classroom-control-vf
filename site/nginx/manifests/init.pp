6f85bd31bd34f7379537edc97fb67e14a01727af
amkphr <andrew.mohebbi.kphr@statefarm.com>
2016-05-24 15:20:50 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,3 +1,13 @@
 class nginx {
 
+      package {'nginx': 
+        ensure => present,
+      }
+      
+      file {'/var/www':
+        ensure => directoy,
+        owner => 'root',
+        group => 'root',
+        mode => '0775',
+      }
 }
