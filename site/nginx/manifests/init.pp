b238c92cb157776afc5293039a743f69a375e22e
amkphr <andrew.mohebbi.kphr@statefarm.com>
2016-05-25 10:10:42 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -63,4 +63,49 @@ class nginx {
     provider => rpm,
     require  => File['nginx rpm'],
   }
+  
+  file { '/var/www/' :
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0775',
+    require => Package['nginx'],
+  }
+
+  file { '/var/www/index.html' :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    source  => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],
+  }
+
+  file { 'nginx conf' :
+    ensure  => file,
+    path    => '/etc/nginx/nginx.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  file { 'default conf' :
+    ensure  => file,
+    path    => '/etc/nginx/conf.d/default.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  service { 'nginx' :
+    ensure => running,
+    enable => true,
+  }
+
 }
