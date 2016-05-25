995f7d66529ae6260b56cc1438aa1eaab64d7077
amkphr <andrew.mohebbi.kphr@statefarm.com>
2016-05-25 11:10:53 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,49 +1,39 @@
 class nginx {
+  Yumrepo {
+    ensure => present,
+    enabled => '1',
+    gpgcheck => '1', 
+    priority -> '99', 
+    skip_if_unavailable => '1', 
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    before     => [ Package['nginx'], Package['openssl-libs'] ],
+  }
+  
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+  }
+  
 
   yumrepo { 'base':
-    ensure              => 'present',
     descr               => 'CentOS-$releasever - Base',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before     => [ Package['nginx'], Package['openssl-libs'] ],
   }
   
   yumrepo { 'updates':
-    ensure              => 'present',
     descr               => 'CentOS-$releasever - Updates',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before     => [ Package['nginx'], Package['openssl-libs'] ],
   }
   
   yumrepo { 'extras':
-    ensure              => 'present',
     descr               => 'CentOS-$releasever - Extras',
-    enabled             => '1',
-    gpgcheck            => '1',
-    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
-    priority            => '99',
-    skip_if_unavailable => '1',
-    before     => [ Package['nginx'], Package['openssl-libs'] ],
   }
   
   yumrepo { 'centosplus':
-    ensure     => 'present',
     descr      => 'CentOS-$releasever - Plus',
-    enabled    => '1',
-    gpgcheck   => '1',
-    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
-    before     => [ Package['nginx'], Package['openssl-libs'] ],
   }
 
   package { [ 'openssl', 'openssl-libs' ] :
@@ -57,7 +47,7 @@ class nginx {
     source   => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
   }
 
-  package { 'nginx' :
+  package { $module_name :
     ensure   => '1.6.2-1.el7.centos.ngx',
     source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
     provider => rpm,
@@ -66,17 +56,11 @@ class nginx {
   
   file { '/var/www/' :
     ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0775',
     require => Package['nginx'],
   }
 
   file { '/var/www/index.html' :
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
     source  => 'puppet:///modules/nginx/index.html',
     require => Package['nginx'],
   }
@@ -84,28 +68,22 @@ class nginx {
   file { 'nginx conf' :
     ensure  => file,
     path    => '/etc/nginx/nginx.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
     source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
   file { 'default conf' :
     ensure  => file,
     path    => '/etc/nginx/conf.d/default.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
     source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
   service { 'nginx' :
     ensure => running,
     enable => true,
+    require => [File['index.html']], 
+    subscribe => [File['nginx conf'], File['default conf']], 
   }
 
 }
