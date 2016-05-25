3524ddd39479d5e4e12278219068f6a107b38170
amkphr <andrew.mohebbi.kphr@statefarm.com>
2016-05-25 08:58:06 -0500

    Update init.pp
    Adding code from Andrew's nginx notes

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,13 +1,14 @@
 class nginx {
+  file { 'nginx rpm' :
+    ensure   => file,
+    path     => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    source   => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+  }
 
-      package {'nginx': 
-        ensure => present,
-      }
-      
-      file {'/var/www':
-        ensure => directoy,
-        owner => 'root',
-        group => 'root',
-        mode => '0775',
-      }
+  package { 'nginx' :
+    ensure   => '1.6.2-1.el7.centos.ngx',
+    source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    provider => rpm,
+    require  => File['nginx rpm'],
+  }
 }
