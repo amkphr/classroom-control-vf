e335d8023f3859be97f44d52f52ad8d5cb221b9a
amkphr <andrew.mohebbi.kphr@statefarm.com>
2016-05-26 15:49:38 -0500

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,6 +1,11 @@
 class profile::apache{
  include ::apache
  include ::apache::mod::php
+ $wp_home = '/opt/wordpress'
+ $blog_name = 'blog city' 
+ 
+ host { "$blog_name}.puppetlabs.com"
+ } 
  # class { 
  # mysql 
  # } 
